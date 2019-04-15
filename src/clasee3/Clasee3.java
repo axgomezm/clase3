4c69d8b2784ea1555e1258ac039e8f509e9ad2f2
axgomezm <axgomezm@BE401S103C29ING.unal.loc>
2019-04-15 07:44:29 -0500

    Clase1.1

---
+++ src/clasee3/Clasee3.java
@@ -0,0 +1,8 @@
+
+package clasee3;
+public class Clasee3 {
+    public static void main(String[] args) {
+        Perro pl= new Perro("F",1);
+        System.out.println(pl.getNombre());
+    }    
+}
---
+++ src/clasee3/Perro.java
@@ -0,0 +1,47 @@
+
+package clasee3;
+
+public class Perro {
+    private double peso;
+    private String color;
+    private String nombre;
+    private int edad;
+
+    public Perro(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+}
